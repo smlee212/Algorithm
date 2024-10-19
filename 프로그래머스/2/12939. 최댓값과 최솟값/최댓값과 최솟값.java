91ee92e7122a31a1f36bd51770006e0759c93320
smlee212 <63399455+smlee212@users.noreply.github.com>
2024-10-19 23:01:55 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 14.75 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String[] arr = s.split(" ");
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for(String str : arr) {
+            int num;
+            if(str.charAt(0)=='-'){
+                String numStr = str.substring(1);
+                num = -Integer.parseInt(numStr);
+            }
+            else
+                num = Integer.parseInt(str);
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            
+        }
+        
+        return min+" "+max;
+    }
+}
