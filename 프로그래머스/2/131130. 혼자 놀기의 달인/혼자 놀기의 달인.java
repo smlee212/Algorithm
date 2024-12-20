7d7736197c40657016ab3046944a2adc3af67cd4
smlee212 <63399455+smlee212@users.noreply.github.com>
2024-12-20 23:13:13 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.46 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -2,46 +2,29 @@ import java.util.*;
 
 class Solution {
     
-    List<List<Integer>> cardGroups;
-    boolean[] visited;
-    int N;
-    
     public int solution(int[] cards) {        
-        N = cards.length;
-        visited = new boolean[N];
-        cardGroups = new ArrayList<>();
+        int N = cards.length;
+        boolean[] visited = new boolean[N];
+        List<Integer> cardGroups = new ArrayList<>();
         
         for(int i=0;i<N;i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                List<Integer> group = new ArrayList<>();
-                group.add(cards[i]);
-                dfs(cards, cards[i]-1, group);
+            int now = i;
+            int cnt = 0;
+            while(!visited[now]) {
+                cnt++;
+                visited[now] = true;
+                now = cards[now] - 1;
             }
+            if(cnt > 0) cardGroups.add(cnt);
         }
         
-        List<Integer> result = new ArrayList<>();        
-        for(List<Integer> group : cardGroups) {
-            result.add(group.size());
-        }
-        Collections.sort(result, Comparator.reverseOrder());   
+        Collections.sort(cardGroups, Comparator.reverseOrder());   
         
-        if(result.size() >= 2) {
-            return result.get(0) * result.get(1);
+        if(cardGroups.size() >= 2) {
+            return cardGroups.get(0) * cardGroups.get(1);
         }
         else {        
             return 0;
-        }
-    }
-    
-    void dfs(int[] cards, int now, List<Integer> group) {
-        if(!visited[now]) {
-            visited[now] = true;
-            group.add(cards[now]);
-            dfs(cards, cards[now]-1, group);
-        }
-        else {
-            cardGroups.add(group);
         }
     }
 }
