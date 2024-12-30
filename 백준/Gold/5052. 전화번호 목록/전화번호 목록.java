8fc9466e2df8d9472306920cd5aa163d998d6cc4
smlee212 <63399455+smlee212@users.noreply.github.com>
2024-12-30 12:34:47 +0900

    [Gold IV] Title: 전화번호 목록, Time: 548 ms, Memory: 30400 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -10,22 +10,12 @@ public class Main {
         int t = Integer.parseInt(br.readLine());
         for(int tc=0;tc<t;tc++) {
             int n = Integer.parseInt(br.readLine());
-
             String[] number = new String[n];
             for(int i=0;i<n;i++) {
                 number[i] = br.readLine();
             }
 
-            Arrays.sort(number, (o1, o2)-> {
-                int index = 0;
-                while(index < o1.length() && index < o2.length()) {
-                    if(o1.charAt(index) != o2.charAt(index)) {
-                        return o1.charAt(index) - o2.charAt(index);
-                    }
-                    index++;
-                }
-                return o1.length() - o2.length();
-            });
+            Arrays.sort(number);
 
             boolean isValid = true;
             for(int i=1;i<n;i++) {
