cf455cf894440a1c5c23c1f3395a3717df51f58a
smlee212 <63399455+smlee212@users.noreply.github.com>
2024-12-30 12:00:47 +0900

    [Gold IV] Title: 도서관, Time: 68 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.java
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -2,51 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        List<Integer> rightBooks = new ArrayList<>();
-        List<Integer> leftBooks = new ArrayList<>();
+        PriorityQueue<Integer> rightBooks = new PriorityQueue<>(Collections.reverseOrder()); // 양수 책
+        PriorityQueue<Integer> leftBooks = new PriorityQueue<>();
 
         int maxAbsBook = 0;
-        boolean isRightOfMaxBookLocation = false;
+
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++) {
             int book = Integer.parseInt(st.nextToken());
+
             if(book > 0) rightBooks.add(book);
             else if(book < 0) leftBooks.add(book);
-            if(Math.abs(book) > maxAbsBook) {
-                maxAbsBook = Math.abs(book);
-                isRightOfMaxBookLocation = book > 0;
+
+            maxAbsBook = Math.max(Math.abs(book), maxAbsBook);
+        }
+
+        int sum = 0;
+        while(!rightBooks.isEmpty()) {
+            sum += 2 * rightBooks.poll();
+            for(int i=0;i<M-1 && !rightBooks.isEmpty();i++) {
+                rightBooks.poll();
+            }
+        }
+        while(!leftBooks.isEmpty()) {
+            sum -= 2 * leftBooks.poll();
+            for(int i=0;i<M-1 && !leftBooks.isEmpty();i++) {
+                leftBooks.poll();
             }
         }
 
-        rightBooks.sort(Comparator.naturalOrder());
-        leftBooks.sort(Comparator.reverseOrder());
-
-        int sum = 0;
-        int rIndex = rightBooks.size()-1;
-        int lIndex = leftBooks.size()-1;
-        if(isRightOfMaxBookLocation) {
-            sum = rightBooks.get(rightBooks.size()-1);
-            rIndex -= M;
-        }
-        else {
-            sum = -leftBooks.get(leftBooks.size()-1);
-            lIndex -= M;
-        }
-
-        for(;rIndex>=0;rIndex-=M) {
-            sum += 2 * rightBooks.get(rIndex);
-        }
-        for(;lIndex>=0;lIndex-=M) {
-            sum -= 2 * leftBooks.get(lIndex);
-        }
-
-        System.out.println(sum);
+        System.out.println(sum-maxAbsBook);
     }
 }
