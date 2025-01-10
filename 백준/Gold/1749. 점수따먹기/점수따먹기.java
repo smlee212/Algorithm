5da450343e1f170b4f292464d47a8900da93746e
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-01-10 12:37:25 +0900

    [Gold IV] Title: 점수따먹기, Time: 248 ms, Memory: 18812 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -21,25 +21,23 @@ public class Main {
         }
 
         int result = Integer.MIN_VALUE;
-        for(int a=1;a<=N;a++) {
-            for(int b=1;b<=M;b++) {
-                int maxSum = func(prefixSum, N, M, a, b);
-                result = Math.max(result, maxSum);
+        // x1, x2 좌표를 고정
+        for(int x1=1;x1<=M;x1++) {
+            for(int x2=x1;x2<=M;x2++) {
+                int lastSum = 0;
+                // y 좌표를 1부터 늘려가며 높이만 다른 직사각형의 행렬을 지정
+                for(int y=1;y<=N;y++) {
+                    // y행(세로 크기 1) 그리고 x1,x2의 가로를 가진 직사각형의 누적합
+                    int rowSum = prefixSum[y][x2] - prefixSum[y][x1-1] - prefixSum[y-1][x2] + prefixSum[y-1][x1-1];
+
+                    // 이전 row들과 더했을때 커지면 더하고, 작아지면 이번행부터 다시 덧셈 시작
+                    lastSum = Math.max(lastSum + rowSum, rowSum);
+
+                    result = Math.max(result, lastSum);
+                }
             }
         }
 
         System.out.println(result);
-    }
-
-    // a,b 부터 c,d 까지 범위의 합
-    private static int func(int[][] prefixSum, int N, int M, int a, int b) {
-        int maxSum = Integer.MIN_VALUE;
-        for(int c=a;c<=N;c++) {
-            for(int d=b;d<=M;d++) {
-                int sum = prefixSum[c][d] - prefixSum[c][b-1] - prefixSum[a-1][d] + prefixSum[a-1][b-1];
-                maxSum = Math.max(maxSum, sum);
-            }
-        }
-        return maxSum;
     }
 }
