aab4cf3e86b966a7c356717953d327baa5d4954b
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-01-13 13:39:43 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 46.02 ms, Memory: 213 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -2,45 +2,42 @@ import java.util.*;
 
 class Solution {
     
-    // 누적합 문제
-    
     public int solution(int[][] board, int[][] skill) {
-        int answer = 0;
-        
         int N = board.length;
         int M = board[0].length;
-        int[][] sumMap = new int[N+1][M+1];
+        int K = skill.length;
         
-        for(int i=0;i<skill.length;i++) {
-            int r1 = skill[i][1];
-            int c1 = skill[i][2];
-            int r2 = skill[i][3];
-            int c2 = skill[i][4];
-            int degree = skill[i][5] * (skill[i][0] == 1 ? -1 : 1);
+        int[][] prefixSum = new int[N+1][M+1];
+        
+        for(int i=0;i<K;i++) {
+            int type = skill[i][0];
+            int r1 = skill[i][1], c1 = skill[i][2];
+            int r2 = skill[i][3], c2 = skill[i][4];
+            int degree = skill[i][5] * (type == 1 ? -1 : 1);
             
-            sumMap[r1][c1] += degree;
-            sumMap[r2+1][c2+1] += degree;
-            sumMap[r2+1][c1] -= degree;
-            sumMap[r1][c2+1] -= degree;
+            prefixSum[r1][c1] += degree;
+            prefixSum[r1][c2+1] += -degree;
+            prefixSum[r2+1][c1] += -degree;
+            prefixSum[r2+1][c2+1] += degree;
         }
         
+        int cnt = 0;
         for(int i=0;i<=N;i++) {
             for(int j=1;j<=M;j++) {
-                sumMap[i][j] += sumMap[i][j-1];
+                prefixSum[i][j] += prefixSum[i][j-1];
             }
         }
         for(int i=1;i<=N;i++) {
             for(int j=0;j<=M;j++) {
-                sumMap[i][j] += sumMap[i-1][j];
+                prefixSum[i][j] += prefixSum[i-1][j];
             }
         }
         for(int i=0;i<N;i++) {
             for(int j=0;j<M;j++) {
-                board[i][j] += sumMap[i][j];
-                if(board[i][j] > 0) answer++;
+                if(board[i][j] + prefixSum[i][j] > 0) cnt++;
             }
         }
         
-        return answer;
+        return cnt;
     }
 }
