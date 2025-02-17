d6dd35b9c7617378e945e08e6a8f942f2a3afc31
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-02-17 13:58:19 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 33.20 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,92 +2,43 @@ import java.util.*;
 
 class Solution {
     
-    private List<Integer>[] graph;
-    private boolean isCycle, isDoughnut;
-    
-    public int[] solution(int[][] edges) {
-        int[] answer = new int[4];
-        
-        int maxNode = 0;
-        
+    public int[] solution(int[][] edges) {        
+        int N = 1000000;
         // 한 정점에서 나가고 들어오는 간선의 개수 체크
-        int[][] inAndOut = new int[1000000][2];
+        int[] in = new int[N+1];
+        int[] out = new int[N+1];
         for(int[] edge : edges) {
             int a = edge[0];
             int b = edge[1];
             
-            inAndOut[a][1]++;
-            inAndOut[b][0]++;
-            
-            maxNode = Math.max(maxNode, Math.max(a,b));
+            out[a]++;
+            in[b]++;
         }
         
-        // 생성한 정점 찾기
-        int targetNode = 0;
-        for(int i=1;i<=maxNode;i++) {
-            // 나가는 간선이 2개 이상이고, 들어오는 간선이 0개
-            if(inAndOut[i][1]>=2 && inAndOut[i][0]==0) {
-                targetNode = i;
-                break;
-            }
-        }
-        answer[0] = targetNode;
+        int targetNode = -1;
+        int stick = 0;
+        int eight = 0;
         
-        // 간선 리스트 생성
-        graph = new ArrayList[maxNode+1];
-        for(int i=0;i<=maxNode;i++) {
-            graph[i] = new ArrayList<>();
-        }
-        
-        // 간선 연결
-        for(int[] edge : edges) {
-            int a = edge[0];
-            int b = edge[1];
-            
-            graph[a].add(b);
-        }
-        
-        // 생성한 정점과 연결된 노드들을 각각 탐색하여 어떤 구조인지 확인
-        boolean[] visited = new boolean[maxNode+1];
-        for(int next : graph[targetNode]) {
-            if(!visited[next]) {     
-                visited[next] = true;
-                isCycle = false;
-                isDoughnut = true;
-                dfs(visited, next, next);
-                
-                if(isCycle) {
-                    // 도넛
-                    if(isDoughnut) {
-                        answer[1]++;
-                    }
-                    // 8자
-                    else {
-                        answer[3]++;
-                    }
+        for(int i=1;i<=N;i++) {
+            // 진출 간선이 2개 이상이고
+            if(out[i] >= 2) { 
+                // 진입 간선이 있을 경우
+                if(in[i] > 0) {
+                    eight++; // 8자 모양
                 }
-                // 막대
+                // 진입 간선이 없을 경우
                 else {
-                    answer[2]++;
+                    targetNode = i; // 생성된 정점
                 }
             }
-        }
-        
-        return answer;
-    }
-    
-    private void dfs(boolean[] visited, int start, int now) {
-        // 나가는 간선이 2개 이상 존재한다면 도넛이 아닌 8차
-        if(graph[now].size() > 1) isDoughnut = false;
-        
-        for(int next : graph[now]) {    
-            if(next == start) { // 시작 노드를 만난다면 순환구조
-                isCycle = true;
-            }
-            if(!visited[next]) {
-                visited[next] = true;
-                dfs(visited, start, next);
+            // 진출 간선이 없고, 진입 간선이 1개라도 있다면
+            else if(out[i] == 0 && in[i] > 0) {
+                stick++; // 막대 모양
+                // 이때, size가 1이여도 생성된 정점으로부터 연결되어 있기 때문에 진입 간선은 존재한다
             }
         }
+        
+        // 생성된 정점과 연결된 노드의 개수는 전체 그래프의 개수
+        return new int[]{targetNode, out[targetNode]-stick-eight, stick, eight};
     }    
 }
