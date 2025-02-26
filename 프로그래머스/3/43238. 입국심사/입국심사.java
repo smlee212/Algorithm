c61dd965dc8f4ee97a74cdece8a0e86f69fcb6a8
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-02-26 19:00:42 +0900

    [level 3] Title: 입국심사, Time: 89.94 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,24 +1,24 @@
 class Solution {
-    public long solution(int n, int[] times) {
-        int temp = 0;
+    
+    public long solution(int n, int[] times) {  
+        // 심사관 별로 최대 심사 시간 구하기
+        long maxTime = 0;
         for(int time : times) {
-            temp = Math.max(temp, time);
+            maxTime = Math.max(maxTime, time);
         }
         
-        long left = 0;
-        long right = (long)(n / times.length) *  (long)temp;
+        long left = 0; 
+        long right = n * maxTime; // n명 모두 최대 심사 시간
         long mid = 0;
-        
         while(left < right) {
             mid = (left + right) / 2;
-            long cnt = 0;
             
+            long cntPeople = 0; // 심사 받은 사람 수
             for(int time : times) {
-                cnt += mid / time;
+                cntPeople += mid / time; // 각 심사관별로 심사 받은 사람 수 더하기
             }
             
-            
-            if(cnt < n) {
+            if(cntPeople < n) {
                 left = mid + 1;
             }
             else {
