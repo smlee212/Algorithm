26d01ce84e969df7cd607a531b96ff0bb6526de3
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-03-06 10:43:39 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,52 +1,47 @@
 class Solution {
     
-    boolean[] visited;
-    int minCnt;
+    int minCnt = Integer.MAX_VALUE;
     
     public int solution(String begin, String target, String[] words) {
-        minCnt = Integer.MAX_VALUE;
-        visited = new boolean[words.length];
+        boolean[] visited = new boolean[words.length];
         
         for(int i=0;i<words.length;i++) {
-            String word = words[i];
-            if(!visited[i] && check(word, begin)) {
+            if(!visited[i] && isValid(begin, words[i])){
                 visited[i] = true;
-                dfs(words, 1, word, target);    
-                visited[i] = false;                
+                dfs(words[i], target, words, visited, 1);
+                visited[i] = false;
             }
-        }        
+        }
         
-        return minCnt==Integer.MAX_VALUE ? 0 : minCnt ;
+        return minCnt == Integer.MAX_VALUE ? 0 : minCnt;
     }
     
-    boolean check(String a, String b) {
+    void dfs(String begin, String target, String[] words, boolean[] visited, int cnt) {
+        if(begin.equals(target)) {
+            minCnt = Math.min(minCnt, cnt);
+            return;
+        }
+        if(minCnt <= cnt) {
+            return;
+        }
+        
+        for(int i=0;i<words.length;i++) {
+            if(!visited[i] && isValid(begin, words[i])){
+                visited[i] = true;
+                dfs(words[i], target, words, visited, cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    boolean isValid(String a, String b) {
         int cnt = 0;
         for(int i=0;i<a.length();i++) {
             if(a.charAt(i)!=b.charAt(i)) {
                 cnt++;
             }
+            if(cnt>=2) return false;
         }
-        
-        if (cnt != 1) return false;
-        else return true;
-    }
-    
-    void dfs(String[] words, int cnt, String now, String target) {
-        if(now.equals(target)) {
-            minCnt = Math.min(minCnt, cnt);
-            return;            
-        }
-        else if(minCnt < cnt) {
-            return;
-        }
-        
-        for(int i=0;i<words.length;i++) {
-            String word = words[i];
-            if(!visited[i] && check(word, now)) {
-                visited[i] = true;
-                dfs(words, cnt+1, word, target);    
-                visited[i] = false;                
-            }
-        }        
+        return true;
     }
 }
