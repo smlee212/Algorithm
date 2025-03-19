bbe9fbb29a09b1dc8339b1b07ec9e6efe01bc111
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-03-19 19:15:05 +0900

    [Gold IV] Title: 테트로미노, Time: 540 ms, Memory: 32092 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -6,8 +6,8 @@ public class Main {
     private static int N, M;
     private static int[][] map;
     private static boolean[][] visited;
-    private static final int[] dy = {-1,0,1,0};
-    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1};
+    private static final int[] dx = {0,1,0};
     private static int maxSum;
 
     public static void main(String[] args) throws IOException {
@@ -31,7 +31,7 @@ public class Main {
             정사각형 4개를 이어붙인 것이 테트로미노이라는 것을 인지하면 된다.
             즉, 시작점에서 4칸 움직인 결과가 테트로미노를 놓은 것과 같다는 것이다
 
-            하지만, T모양의 경우 dfs로는 구할 수 없으므로 추가적으로 계산해준다.
+            근데, 단순한 dfs의 경우 T모양을 만들 수 없으므로, 이동하지 않고 해당 방향의 점수만 먹는 로직을 추가함
          */
 
         visited = new boolean[N][M];
@@ -39,36 +39,13 @@ public class Main {
 
         for(int i=0;i<N;i++) {
             for(int j=0;j<M;j++) {
-                // dfs
                 visited[i][j] = true;
                 dfs(i,j,1, map[i][j]);
                 visited[i][j] = false;
-                // t모양
-                tPattern(i,j);
             }
         }
 
         System.out.println(maxSum);
-    }
-
-    private static void tPattern(int y, int x) {
-        int sum = 0;
-        if(isValid(y,x-1) && isValid(y,x+1) && isValid(y+1,x)) {
-            sum = map[y][x] + map[y][x-1] + map[y][x+1] + map[y+1][x];
-            maxSum = Math.max(maxSum,sum);
-        }
-        if(isValid(y,x-1) && isValid(y,x+1) && isValid(y-1,x)) {
-            sum = map[y][x] + map[y][x-1] + map[y][x+1] + map[y-1][x];
-            maxSum = Math.max(maxSum,sum);
-        }
-        if(isValid(y+1,x) && isValid(y-1,x) && isValid(y,x+1)) {
-            sum = map[y][x] + map[y+1][x] + map[y-1][x] + map[y][x+1];
-            maxSum = Math.max(maxSum,sum);
-        }
-        if(isValid(y+1,x) && isValid(y-1,x) && isValid(y,x-1)) {
-            sum = map[y][x] + map[y+1][x] + map[y-1][x] + map[y][x-1];
-            maxSum = Math.max(maxSum,sum);
-        }
     }
 
     private static void dfs(int y, int x, int cnt, int sum) {
@@ -77,15 +54,21 @@ public class Main {
             return;
         }
 
-        for(int i=0;i<4;i++) {
+        // 위, 아래, 오른쪽 3방향에 대해서만 탐색을 진행
+        for(int i=0;i<3;i++) {
+            // 첫번째에서 위로 이동은 패스
+            if(cnt==1 && i==0) continue;
+
             int ny = y + dy[i];
             int nx = x + dx[i];
 
-            if(!isValid(ny,nx)) continue;
-
-            if(!visited[ny][nx]) {
+            if(isValid(ny,nx) && !visited[ny][nx]) {
                 visited[ny][nx] = true;
+                // 해당 방향으로 이동
                 dfs(ny,nx,cnt+1,sum+map[ny][nx]);
+                // 이동하지 않고 해당 방향의 점수만 먹기
+                if(cnt<3)
+                    dfs(y,x,cnt+1,sum+map[ny][nx]);
                 visited[ny][nx] = false;
             }
         }
