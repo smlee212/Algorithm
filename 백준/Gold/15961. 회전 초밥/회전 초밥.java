58252c723fcf238da4e8042d81aa6b90d8117465
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-04-04 16:58:32 +0900

    [Gold IV] Title: 회전 초밥, Time: 476 ms, Memory: 168724 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,76 +1,63 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String args[]) throws Exception {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    StringTokenizer st;
-	    
-	    st = new StringTokenizer(br.readLine());
-	    
-	    int N = Integer.parseInt(st.nextToken());
-	    int d = Integer.parseInt(st.nextToken());
-	    int k = Integer.parseInt(st.nextToken());
-	    int c = Integer.parseInt(st.nextToken());
-	   
-	    int[] sushi = new int[N]; // 회전 초밥 배열
-	    int[] selected = new int[d+1]; // 1번부터 d번까지 초밥 중 선택한 개수
-	    
-	    for(int i=0;i<N;i++) {
-	    	sushi[i] = Integer.parseInt(br.readLine());
-	    }
-	    
-	    int res = 0, // 초밥 가짓수의 최대값
-	    	now = 0; // 현재 선택한 초밥 가짓수
-	    
-	    // 초기 윈도우
-	    for(int i=0;i<k;i++) {
-	    	// 새롭게 선택한 초밥이라면 가짓수 증가
-	    	if(selected[sushi[i]] == 0) {
-	    		res++;
-	    	}
-	    	// 선택한 초밥 개수 증가
-	    	selected[sushi[i]]++;
-	    }
-	    
-	    // 초기 윈도우 설정
-	    now = res;
-	    
-	    // 쿠폰 초밥 고려
-	    if(selected[c] == 0) {
-	    	res++;
-	    }
-	    
-	    // 윈도우를 한칸씩 슬라이딩하며 가짓수 고려
-	    for(int i=1;i<N;i++) {
-	    	// 이전 초밥을 윈도우에서 제거
-	    	selected[sushi[i-1]]--;
-	    	
-	    	// 이전 초밥이 하나뿐이였다면 가짓수 제거
-	    	if(selected[sushi[i-1]] == 0) {
-	    		now--;
-	    	}
-	    	
-	    	// 슬라이딩으로 선택한 초밥이 새롭게 선택했다면 가짓수 추가 
-	    	if(selected[sushi[(i+k-1)%N]] == 0) {
-	    		now++;
-	    	}
-	    	// 선택한 초밥 개수 증가
-	    	selected[sushi[(i+k-1)%N]]++;
-	    	
-	    	// 쿠폰 초밥 고려
-	    	if(selected[c]==0) {
-	    		res = Math.max(res, now+1);
-	    	}
-	    	else {
-	    		res = Math.max(res, now);
-	    	}
-	    }	    
-	    
-	    System.out.println(res);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int[] map = new int[N+k-1];
+        for(int i=0;i<N;i++) {
+            map[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i=0;i<k-1;i++) {
+            map[i+N] = map[i];
+        }
+
+        int[] cntSushi = new int[d+1];
+        int numberOfSushi = 0;
+        int maxNumberOfSushi = 0;
+
+        // 초기 세팅
+        for(int i=0;i<k;i++) {
+            int sushi = map[i];
+            // 현재 스시 추가
+            if(cntSushi[sushi] == 0) {
+                numberOfSushi++;
+            }
+            cntSushi[sushi]++;
+        }
+        // 계산
+        maxNumberOfSushi = cntSushi[c] == 0 ? numberOfSushi + 1 : numberOfSushi;
+
+        // 남은 회전초밥에 대해 진행
+        for(int i=k;i<map.length;i++) {
+            int lastSushi = map[i];
+            int firstSushi = map[i-k];
+            // 현재 스시 추가
+            if(cntSushi[lastSushi] == 0) {
+                numberOfSushi++;
+            }
+            cntSushi[lastSushi]++;
+
+            // 처음 스시 제거
+            cntSushi[firstSushi]--;
+            if(cntSushi[firstSushi] == 0) {
+                numberOfSushi--;
+            }
+
+            // 계산
+            maxNumberOfSushi = Math.max(maxNumberOfSushi, cntSushi[c] == 0 ? numberOfSushi + 1 : numberOfSushi);
+        }
+
+        System.out.println(maxNumberOfSushi);
+    }
 }
