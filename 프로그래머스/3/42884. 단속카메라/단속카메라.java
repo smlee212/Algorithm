2a36c051551a0ab85e18047dbf91fd49a1fc4b0b
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-04-08 18:49:37 +0900

    [level 3] Title: 단속카메라, Time: 10.12 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 class Solution {
-    // 6:40
     public int solution(int[][] routes) {
         
         // 진출 시간을 기준으로 오름차순 정렬
@@ -9,18 +8,12 @@ class Solution {
             return o1[1] - o2[1];
         });
         
+        int lastTime = Integer.MIN_VALUE;
         int cnt = 0;
-        boolean[] visited = new boolean[routes.length];
-        
-        for(int i=0;i<routes.length;i++) {
-            if(!visited[i]) {
-                visited[i] = true;
+        for(int[] route : routes) {
+            if(lastTime < route[0]) {
+                lastTime = route[1];
                 cnt++;
-                for(int j=i+1;j<routes.length;j++) {
-                    if(routes[j][0] <= routes[i][1] && routes[i][1] <= routes[j][1]) {
-                        visited[j] = true;
-                    }
-                }
             }
         }
         
