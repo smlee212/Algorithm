e7108befa8dda601e97894b5eb9a3d3a33eb33ab
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-04-09 13:03:26 +0900

    [level 3] Title: 가장 먼 노드, Time: 44.62 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,46 +3,53 @@ import java.util.*;
 class Solution {
     
     public int solution(int n, int[][] edge) {
-        // 인접 리스트 초기화
+        // 12:45
+        
+        // 간선 리스트
         List<List<Integer>> graph = new ArrayList<>();
         for(int i=0;i<=n;i++) {
-           graph.add(new ArrayList<>()); 
+            graph.add(new ArrayList<>());
         }
-            
+        
         for(int[] e : edge) {
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
         
-        int[] visited = new int[n+1];
-        for(int i=0;i<=n;i++) {
-            visited[i] = Integer.MAX_VALUE;
+        int[] dist = new int[n+1];
+        boolean[] visited = new boolean[n+1];
+        
+        final int INF = Integer.MAX_VALUE;
+        Arrays.fill(dist,INF);
+        
+        dist[1] = 0;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->o1[1]-o2[1]);
+        pq.add(new int[]{1,0});
+        
+        while(!pq.isEmpty()) {            
+            int nowNode = pq.poll()[0];
+            
+            if(visited[nowNode]) continue;
+            visited[nowNode] = true;
+            
+            for(int nextNode : graph.get(nowNode)) {                
+                if(dist[nextNode] > dist[nowNode] + 1) {
+                    dist[nextNode] = dist[nowNode] + 1;
+                    pq.add(new int[]{nextNode, dist[nowNode] + 1});
+                }
+            }
         }
         
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.add(new int[]{1,0});
-        
-        int maxDist = 0;
+        int maxDist = -1;
         int cnt = 0;
-        while(!dq.isEmpty()) {
-            int[] temp = dq.poll();
-            int now = temp[0];
-            int dist = temp[1];
-            System.out.println(now+", "+dist);
-            
-            if(maxDist < dist) {
-                maxDist = dist;
+        for(int i=2;i<=n;i++) {
+            if(maxDist < dist[i]) {
+                maxDist = dist[i];
                 cnt = 1;
             }
-            else if(maxDist == dist) {
+            else if(maxDist == dist[i]) {
                 cnt++;
-            }
-            
-            for(int next : graph.get(now)) {
-                if(next!=1 && dist + 1 < visited[next]) {
-                    visited[next] = dist + 1;
-                    dq.add(new int[]{next, dist+1});
-                }
             }
         }
         
