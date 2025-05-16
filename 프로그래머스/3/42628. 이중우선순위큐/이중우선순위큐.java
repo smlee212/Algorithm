1a5defc1f7c9901483befc2bfa83166b52db48b3
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-05-16 14:39:52 +0900

    [level 3] Title: 이중우선순위큐, Time: 136.82 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,16 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] operations) {
+    
+    // 2:20
+    
+    public int[] solution(String[] operations) {      
         LinkedList<Integer> list = new LinkedList<>();
-            
-        for(String s : operations) {
-            if(s.charAt(0)=='I') {
-                int num = Integer.parseInt(s.substring(2));
-                // 숫자 삽입
+        
+        for(String op : operations) {
+            String[] temp = op.split(" ");
+            int num = Integer.parseInt(temp[1]);
+            if(temp[0].equals("I")) {
                 int index = 0;
-                for(Integer e : list) {
-                    if(e < num) {
+                for(Integer item : list) {
+                    if(item < num) {
                         index++;
                     }
                     else {
@@ -19,36 +22,21 @@ class Solution {
                 }
                 list.add(index, num);
             }
-            else if(!list.isEmpty()) {
-                // 최소값 삭제
-                if(s.charAt(2)=='-') {
+            else if(!list.isEmpty()){
+                if(num < 0) {
                     list.removeFirst();
                 }
-                // 최대값 삭제
                 else {
                     list.removeLast();
                 }
             }
         }
         
-        int[] answer = new int[2];
         if(list.isEmpty()) {
-            answer[0] = 0;
-            answer[1] = 0;
+            return new int[]{0,0};
         }
         else {
-            answer[0] = list.get(list.size()-1);
-            answer[1] = list.get(0);
+            return new int[]{list.get(list.size()-1), list.get(0)};
         }
-        return answer;
     }
-    
-    void print(LinkedList<Integer> list) {
-        for(Integer i : list) {
-            System.out.print(i+" ");
-        }
-        System.out.println();
-    }
-    
-    
 }
