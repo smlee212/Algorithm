1ab048f52646951b1cd6c571c6a45aede0f3ef5f
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-05-26 14:22:11 +0900

    [level 4] Title: 도둑질, Time: 50.20 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -3,24 +3,28 @@ class Solution {
     public int solution(int[] money) {
         int n = money.length;
         // i번째 집을 털었을 때 최대값
-        int[] dpFirst = new int[n]; // 첫번째 집을 털었을 경우
-        int[] dpNotFirst = new int[n]; // 첫번째 집을 털지 않았을 경우
+        // j = 0 이면 i-1번째 집을 털지 않았을때 최대값 / j = 1 이면 i-1번째 집을 털었을때 최대값
+        int[][] dpFirst = new int[2][n]; // 첫번째 집을 털었을 경우
+        int[][] dpNotFirst = new int[2][n]; // 첫번째 집을 털지 않았을 경우
         
-        // 초기값 설정
-        dpFirst[0] = money[0];
-        dpFirst[1] = Math.max(money[0], money[1]);
-        dpNotFirst[0] = 0;
-        dpNotFirst[1] = money[1];
+        // 0번째 집을 털었을 경우에 대해 초기값 설정
+        dpFirst[1][0] = money[0];
         
-        for(int i=2;i<n-1;i++) {
-            dpFirst[i] = Math.max(dpFirst[i-2] + money[i], dpFirst[i-1]);
-            dpNotFirst[i] = Math.max(dpNotFirst[i-2] + money[i], dpNotFirst[i-1]);
+        for(int i=1;i<n-1;i++) {
+            dpFirst[0][i] = Math.max(dpFirst[1][i-1], dpFirst[0][i-1]);
+            dpFirst[1][i] = dpFirst[0][i-1] + money[i];
+            dpNotFirst[0][i] = Math.max(dpNotFirst[1][i-1], dpNotFirst[0][i-1]);
+            dpNotFirst[1][i] = dpNotFirst[0][i-1] + money[i];
         }        
-        // 첫번째 집을 털었기 때문에, 마지막 집은 털 수 없다
-        dpFirst[n-1] = Math.max(dpFirst[n-3], dpFirst[n-2]);
-        // 첫번째 집을 털지 않았기에, 마지막 집을 털 수 있다
-        dpNotFirst[n-1] = Math.max(dpNotFirst[n-3] + money[n-1], dpNotFirst[n-2]);
+        
+        // 첫번째 집을 털었던 dpFirst의 경우, 마지막 집은 털수 없다
+        int resultFirst = Math.max(dpFirst[0][n-2], dpFirst[1][n-2]);        
+        
+        // 첫번째 집을 털지 않은 dpNotFirst의 경우, 마지막 집을 털수 있다
+        dpNotFirst[0][n-1] = Math.max(dpNotFirst[1][n-2], dpNotFirst[0][n-2]);
+        dpNotFirst[1][n-1] = dpNotFirst[0][n-2] + money[n-1];        
+        int resultNotFirst = Math.max(dpNotFirst[0][n-1], dpNotFirst[1][n-1]);
                 
-        return Math.max(dpFirst[n-1], dpNotFirst[n-1]);
+        return Math.max(resultFirst, resultNotFirst);
     }
 }
