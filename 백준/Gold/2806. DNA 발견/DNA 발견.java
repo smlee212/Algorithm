9e9caa0a32dc234d7e7a3e1ed7cb586936510080
smlee212 <63399455+smlee212@users.noreply.github.com>
2025-08-29 22:57:49 +0900

    [Gold IV] Title: DNA 발견, Time: 160 ms, Memory: 30536 KB -BaekjoonHub

--- 백준/Gold/2806. DNA 발견/DNA 발견.java
+++ 백준/Gold/2806. DNA 발견/DNA 발견.java
@@ -8,22 +8,22 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         char[] dna = br.readLine().toCharArray();
 
-        int[][] dp = new int[N][2]; // dp[i][j] : i번째 문자를 변형/유지하여 j(0:모두A, 1:모두B)가 되기 위한 최소 개수
+        int[][] dp = new int[2][N]; // dp[i][j] : j번째 문자를 변형/유지하여 i(0:모두A, 1:모두B)가 되기 위한 최소 개수
         dp[0][0] = dna[0] == 'A' ? 0 : 1;
-        dp[0][1] = dna[0] == 'B' ? 0 : 1;
+        dp[1][0] = dna[0] == 'B' ? 0 : 1;
 
-        for(int i=1;i<N;i++) {
-            if(dna[i] == 'A') {
-                dp[i][0] = dp[i-1][0]; // i번째(A)를 유지하고 모두 A로 만들기
-                dp[i][1] = Math.min(dp[i-1][0]+1, dp[i-1][1]+1); // 모두A를 변형하기 또는 i번째(A)를 B로 변경하여 모두 B로 만들기
+        for(int j=1;j<N;j++) {
+            if(dna[j] == 'A') {
+                dp[0][j] = dp[0][j-1]; // j번째(A)를 유지하고 모두 A로 만들기
+                dp[1][j] = Math.min(dp[0][j-1]+1, dp[1][j-1]+1); // 모두A를 변형하기 또는 j번째(A)를 B로 변경하여 모두 B로 만들기
             }
             else {
-                dp[i][0] = Math.min(dp[i-1][1]+1, dp[i-1][0]+1); // 모두B를 변형하기 또는 i번째(B)를 A로 변경하여 모두 A로 만들기
-                dp[i][1] = dp[i-1][1]; // i번째(B)를 유지하고 모두 B로 만들기
+                dp[0][j] = Math.min(dp[1][j-1]+1, dp[0][j-1]+1); // 모두B를 변형하기 또는 j번째(B)를 A로 변경하여 모두 A로 만들기
+                dp[1][j] = dp[1][j-1]; // j번째(B)를 유지하고 모두 B로 만들기
             }
         }
 
         // (모두 A 만들기 경우) 또는 (모두 B 만들기 경우에서 전부 변형)
-        System.out.println(Math.min(dp[N-1][0], dp[N-1][1] + 1));
+        System.out.println(Math.min(dp[0][N-1], dp[1][N-1] + 1));
     }
 }
